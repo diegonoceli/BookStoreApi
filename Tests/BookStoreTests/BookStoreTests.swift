1ec425ad392bbf76bd652a59b4ee5b771d468943
Diego Noceli <diegonoceli@gmail.com>
2023-08-26 20:08:56 -0300

    creating lib

--- Tests/BookStoreTests/BookStoreTests.swift
+++ Tests/BookStoreTests/BookStoreTests.swift
@@ -2,10 +2,32 @@ import XCTest
 @testable import BookStore
 
 final class BookStoreTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(BookStore().text, "Hello, World!")
+    
+    func testFetchBooks() {
+            let expectation = self.expectation(description: "Fetch books")
+            
+            let api = BookStore.shared
+            api.fetchBooks(query: "ios", maxResults: 10, startIndex: 0) { result in
+                switch result {
+                case .success(let books):
+                    XCTAssertFalse(books.isEmpty, "Fetched books should not be empty")
+                    expectation.fulfill()
+                case .failure(let error):
+                    XCTFail("Error: \(error.localizedDescription)")
+                }
+            }
+            
+            waitForExpectations(timeout: 10, handler: nil)
+        }
+    
+    func testBookInfoParsing() {
+        let bookInfo = BookInfo(volumeInfo: BookDetails(title: "Test Book", authors: ["Author"], description: "Description", imageLinks: nil, buyLink: nil))
+        let book = bookInfo.toBook()
+        
+        XCTAssertEqual(book.title, "Test Book")
+        XCTAssertEqual(book.authors, ["Author"])
+        XCTAssertEqual(book.description, "Description")
+        XCTAssertNil(book.thumbnailURL)
+        XCTAssertNil(book.buyLink)
     }
 }
